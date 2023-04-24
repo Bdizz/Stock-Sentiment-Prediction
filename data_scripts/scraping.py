79e8844658d1095e658a5bc5a3c0344e1fbc8c66
Brandon <Bdzrox01@gmail.com>
2023-04-23 22:53:28 -0400

    made a TODO.txt file

--- data_scripts/scraping.py
+++ data_scripts/scraping.py
@@ -8,7 +8,7 @@ from textblob import TextBlob
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-url = "https://finance.yahoo.com/rss/headline?s=LUMN"
+url = "https://finance.yahoo.com/rss/headline?s=MDWT"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
 
 response = requests.get(url, headers=headers)
